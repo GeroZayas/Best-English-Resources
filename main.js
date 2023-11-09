d508093136d30600897f7f8fb46771f5bd062a72
Gero Zayas <gerozayas@gmail.com>
2023-11-09 16:31:41 +0100

    Add Vocab logic

--- main.js
+++ main.js
@@ -238,3 +238,34 @@ fetch("/data/pronunciation.csv")
     }
   });
 s;
+
+// PRONUNCIATION DATA
+
+const olVocab = document.getElementById("vocab");
+
+fetch("/data/vocab.csv")
+  .then((response) => response.text())
+  .then((data) => {
+    const rows = data.split("\n");
+    const headers = rows[0].split(",");
+    for (let i = 1; i < rows.length; i++) {
+      const cols = rows[i].split(",");
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const p = document.createElement("p");
+      const ranking = document.createElement("p");
+
+      a.href = cols[0];
+      a.innerHTML = "<b class='links'>" + cols[1] + "</b>";
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+      p.textContent = cols[2];
+      ranking.textContent = cols[3];
+
+      li.appendChild(a);
+      li.appendChild(p);
+      li.appendChild(ranking);
+      olVocab.appendChild(li);
+    }
+  });
+s;
