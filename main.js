f4d38a9798d268c9c504c985ffd24fe91c92fafc
Gero Zayas <gerozayas@gmail.com>
2023-11-16 10:51:29 +0100

    Add FCE links

--- main.js
+++ main.js
@@ -375,3 +375,63 @@ fetch("/data/grammar.csv")
       olGrammar.appendChild(li);
     }
   });
+
+// FCE DATA
+
+const olFce = document.getElementById("fce");
+
+fetch("/data/fce.csv")
+  .then((response) => response.text())
+  .then((data) => {
+    const rows = data.split("\n");
+    const headers = rows[0].split(",");
+    for (let i = 1; i < rows.length; i++) {
+      const cols = rows[i].split(",");
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const p = document.createElement("p");
+      const ranking = document.createElement("p");
+
+      a.href = cols[0];
+      a.innerHTML = "<b class='links'>" + cols[1] + "</b>";
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+      p.textContent = cols[2];
+      ranking.textContent = cols[3];
+
+      li.appendChild(a);
+      li.appendChild(p);
+      li.appendChild(ranking);
+      olFce.appendChild(li);
+    }
+  });
+
+// CAE DATA
+
+const olCae = document.getElementById("cae");
+
+fetch("/data/cae.csv")
+  .then((response) => response.text())
+  .then((data) => {
+    const rows = data.split("\n");
+    const headers = rows[0].split(",");
+    for (let i = 1; i < rows.length; i++) {
+      const cols = rows[i].split(",");
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const p = document.createElement("p");
+      const ranking = document.createElement("p");
+
+      a.href = cols[0];
+      a.innerHTML = "<b class='links'>" + cols[1] + "</b>";
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+      p.textContent = cols[2];
+      ranking.textContent = cols[3];
+
+      li.appendChild(a);
+      li.appendChild(p);
+      li.appendChild(ranking);
+      olCae.appendChild(li);
+    }
+  });
